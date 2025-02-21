a067f484fe7b5887bcb97b7c3916396d5bc2ad1e
huyandres <huyandres2001@gmail.com>
2025-02-22 01:06:44 +0700

    add actuator

--- src/main/java/com/bansikah/bankingapp/config/SwaggerConfig.java
+++ src/main/java/com/bansikah/bankingapp/config/SwaggerConfig.java
@@ -5,13 +5,28 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI customOpenAPI() throws SocketException {
+        AtomicReference<String> address = new AtomicReference<>("");
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        networkInterfaces.asIterator().forEachRemaining(networkInterface -> {
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            inetAddresses.asIterator().forEachRemaining(inetAddress -> {
+                String hostAddress = inetAddress.getHostAddress();
+                address.set(address.get() + hostAddress + ", ");
+            });
+        });
         return new OpenAPI()
-                .info(new Info().title("Bank Account API hihi wecommit mai dinh kakakaka")
+                .info(new Info().title("Bank Account API hihi wecommit mai dinh kakakaka ip = " + address.get())
                         .version("1.0")
                         .description("API to test CRUD Bank Account | Tech with Bansikah"));
     }
